43d12941d4a56ad5405749cf10e48698d373c5ab
Kevin Grote <kevin.grote.ltd@gmail.com>
2024-03-08 22:46:16 +0200

    feat(GameTimer): Add first parallelism

--- src/main/kotlin/com/cookingGame/application/GameService.kt
+++ src/main/kotlin/com/cookingGame/application/GameService.kt
@@ -4,30 +4,38 @@ import com.cookingGame.LOGGER
 import com.cookingGame.domain.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consume
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.plus
 
 class GameService {
-    private val job = Job()
-    private val coroutineScope = CoroutineScope(job + Dispatchers.Default)
+    private val timerScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
     fun startGameTimer(gameId: GameId, startTime: GameStartTime, gameDuration: GameDuration): Flow<GameCommand> = flow {
         val endTime = startTime.value.plus(gameDuration.value.longValueExact(), DateTimeUnit.SECOND)
         LOGGER.info("End time: $endTime")
-        var clock = Clock.System.now()
-        while (clock < endTime) {
-            clock = Clock.System.now()
-            if (clock > endTime) break
-        }
-        emit(CheckGameTimerCommand(gameId))
-        LOGGER.info("Command emitted: $clock")
-    }
 
-    fun stopGame(gameId: GameId) {
-        job.cancel() // Stops the coroutine when the game stops
+        val channel = Channel<GameCommand>()
+
+        timerScope.launch {
+            var clock = Clock.System.now()
+            while (clock < endTime) {
+                clock = Clock.System.now()
+                if (clock > endTime) break
+            }
+            channel.send(CheckGameTimerCommand(gameId))
+            LOGGER.info("Command emitted: $clock")
+            channel.close()
+        }
+
+        channel.consume {
+            emit(this.receive())
+        }
     }
 }
