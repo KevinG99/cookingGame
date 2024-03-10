eba6dd43d41c9871e2c356ac35dca3ce86ab1ce9
Kevin Grote <kevin.grote.ltd@gmail.com>
2024-03-10 14:12:06 +0200

    feat(GameTimer): Add parallelism

--- src/main/kotlin/com/cookingGame/application/GameService.kt
+++ src/main/kotlin/com/cookingGame/application/GameService.kt
@@ -2,14 +2,12 @@ package com.cookingGame.application
 
 import com.cookingGame.LOGGER
 import com.cookingGame.domain.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consume
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.plus
@@ -17,25 +15,34 @@ import kotlinx.datetime.plus
 class GameService {
     private val timerScope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
+    private val activeTimers = mutableMapOf<GameId, CompletableJob>()
+
     fun startGameTimer(gameId: GameId, startTime: GameStartTime, gameDuration: GameDuration): Flow<GameCommand> = flow {
         val endTime = startTime.value.plus(gameDuration.value.longValueExact(), DateTimeUnit.SECOND)
-        LOGGER.info("End time: $endTime")
 
         val channel = Channel<GameCommand>()
+        val job = Job(timerScope.coroutineContext[Job])
 
-        timerScope.launch {
+        activeTimers[gameId] = job
+
+        timerScope.launch(job) {
+        LOGGER.info("End time: $endTime, game id: $gameId")
             var clock = Clock.System.now()
-            while (clock < endTime) {
+            while (isActive && clock < endTime) {
                 clock = Clock.System.now()
                 if (clock > endTime) break
             }
             channel.send(CheckGameTimerCommand(gameId))
-            LOGGER.info("Command emitted: $clock")
+            LOGGER.info("Command emitted: $clock, game id: $gameId")
             channel.close()
         }
 
         channel.consume {
-            emit(this.receive())
+            emit(receive())
         }
+    }.onCompletion { activeTimers.remove(gameId) }
+
+    fun stopGameTimer(gameId: GameId) {
+        activeTimers[gameId]?.cancel(CancellationException("Game timer stopped"))
     }
 }
